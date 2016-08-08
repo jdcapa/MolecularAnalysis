e4bc2b11ef020ae4bf44e2bbefd98e574a20343f
Jalo D. Capa <jalo.capa@gmail.com>
2016-08-08 19:53:20 +0200

    Missing comma

--- install.py
+++ install.py
@@ -10,7 +10,7 @@ import sys
 scripts = ["GeoOpt/gopt",
            "XYZ/xyz",
            "HarmonicAnalysis/harmonics",
-           "CoreLevel/corelevel"
+           "CoreLevel/corelevel",
            "InputMultiplicities/inp_mult"]
 
 
